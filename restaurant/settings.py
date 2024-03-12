b67f64238bd7746c6e3cb8512303315ef9a97f3c
saminmahmud <samin.mahmud18@gmail.com>
2024-03-12 21:04:01 +0600

    again cors added

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -34,10 +34,14 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS =['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:5500/']
+CORS_ALLOWED_ORIGINS = [
+    # "http://127.0.0.1:5500/",
+    'https://restaurant-zz0i.onrender.com',
+    'https://*.127.0.0.1',
+]
 
-CSRF_TRUSTED_ORIGINS = ['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:5500/']
-
+# CSRF_TRUSTED_ORIGINS = ['https://restaurant-zz0i.onrender.com','http://127.0.0.1:5500/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:8000/']
 # Application definition
 
 INSTALLED_APPS = [
