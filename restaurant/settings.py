3360c05ead64c758d1a4aa0c43df6fe08214169d
saminmahmud <samin.mahmud18@gmail.com>
2024-03-12 20:54:48 +0600

    cors added

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -34,8 +34,9 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS =['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:5500/']
 
-CORS_ALLOWED_ORIGINS = ['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = ['https://restaurant-zz0i.onrender.com','https://*.127.0.0.1','http://127.0.0.1:5500/']
 
 # Application definition
 
