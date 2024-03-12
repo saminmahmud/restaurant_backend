1b4698a4774b405c14fe6f06cdd2cefb5ff27321
saminmahmud <samin.mahmud18@gmail.com>
2024-03-12 17:30:02 +0600

    confirmation redirect link

--- account/views.py
+++ account/views.py
@@ -54,11 +54,11 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
-        # return HttpResponseRedirect('https://fascinating-seahorse-d8f166.netlify.app/login')
+        # return redirect('login')
+        return HttpResponseRedirect('http://127.0.0.1:5500/login')
     else:
-        return redirect('register')
-        # return HttpResponseRedirect('https://fascinating-seahorse-d8f166.netlify.app/register')
+        # return redirect('register')
+        return HttpResponseRedirect('http://127.0.0.1:5500/register')
     
 
 class UserLoginApiView(APIView):
