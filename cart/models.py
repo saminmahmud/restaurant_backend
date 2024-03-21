b028813bc9ecca327054e5089ad62d65d2c51f4c
saminmahmud <samin.mahmud18@gmail.com>
2024-03-21 13:34:19 +0600

    fixed order

--- cart/models.py
+++ cart/models.py
@@ -12,20 +12,11 @@ class Cart(models.Model):
 
 
 # signals.py
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from order.models import Order
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from order.models import Order
 
 # @receiver(post_save, sender=Cart)
 # def create_order(sender, instance, created, **kwargs):
 #     if created:
-#         order = Order.objects.filter(user=instance.user, status='Pending').first()
-#         if not order:
-#             order = Order.objects.create(user=instance.user)
-#         order.cart = instance
-#         order.save()
-
-@receiver(post_save, sender=Cart)
-def create_order(sender, instance, created, **kwargs):
-    if created:
-        Order.objects.create(user=instance.user, food=instance.food, cart=instance, quantity=instance.quantity, status='Pending')
+#         Order.objects.create(user=instance.user, food=instance.food, cart=instance, quantity=instance.quantity, status='Pending')
